4b0bf05fd7ea3b87fc602b4f1bde279b188e289c
marlon <marlon_veiga@hotmail.com>
2020-09-05 14:15:40 -0300

    Add link at sublists.

--- src/components/Miniatures/index.jsx
+++ src/components/Miniatures/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { references } from "../../utils/references/references";
 import "./style.css";
+import { Link } from "react-router-dom";
+import { urlMask } from "../../utils/helpers/masks";
 
 function Miniatures(props) {
   const { link } = props;
+  //const path = props.location.pathname.substring(1);
 
+  const payload = {
+    pathname: `${urlMask(link)}/${references(link).id}`,
+  };
   //  new RegExp('\\b' + myWord + '\\b')
 
   return link === undefined ? (
@@ -13,12 +19,14 @@ function Miniatures(props) {
     </div>
   ) : (
     <>
-      <img
-        className="avatar"
-        src={references(link).img}
-        alt={references(link).name}
-      />
-      <h6>{references(link).name}</h6>
+      <Link to={payload}>
+        <img
+          className="avatar"
+          src={references(link).img}
+          alt={references(link).name}
+        />
+        <h6>{references(link).name}</h6>
+      </Link>
     </>
   );
 }
