0c73d1ffadad9d24819fd7dcc2ad0d39571c8020
marlon <marlon_veiga@hotmail.com>
2020-09-13 22:31:00 -0300

    Add Styled Components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Routes from "./routes";
-import './styles/global.css'
+import { StylesProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+import "./styles/global.css";
 
 function App() {
-  return <Routes />;
+  return (
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <Routes />;
+    </StylesProvider>
+  );
 }
 
 export default App;
