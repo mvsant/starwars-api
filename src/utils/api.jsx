0802cf2ae7d951e3a5fb47671a2a222ce9cf9e3d
Marlon Veiga <marlon_veiga@hotmail.com>
2020-10-24 16:11:07 -0300

    Test
    referer test..

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -13,7 +13,7 @@ export async function indexFullPath(setData, url) {
 }
 
 export async function nestByOne(setData, parameter) {
-  await fetch(`${baseUrl}${parameter}`)
+  await fetch(`${baseUrl}${parameter}`, {referrerPolicy: 'no-referrer-when-downgrade'})
     .then((response) => response.json())
     .then((data) => setData(data))
 }
